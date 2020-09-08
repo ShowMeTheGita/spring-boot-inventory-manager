a94551afc1ef02c04bfb16bad3c5e22a58f9b156
Guilherme  Silva <guilherme.m.m.f.silva@gmail.com>
2020-09-08 15:30:21 +0100

    Initial commit

---
+++ src/test/java/org/academiadecodigo/floppybirds/InventoryManager/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.floppybirds.InventoryManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
