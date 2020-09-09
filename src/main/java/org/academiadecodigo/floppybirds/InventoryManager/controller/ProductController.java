54a2a2fc5b848a17b1dfeb2458e7ec67eb79a889
Guilherme  Silva <guilherme.m.m.f.silva@gmail.com>
2020-09-09 15:13:01 +0100

    annotation changes on controller

--- src/main/java/org/academiadecodigo/floppybirds/InventoryManager/controller/ProductController.java
+++ src/main/java/org/academiadecodigo/floppybirds/InventoryManager/controller/ProductController.java
@@ -3,11 +3,13 @@ package org.academiadecodigo.floppybirds.InventoryManager.controller;
 import org.academiadecodigo.floppybirds.InventoryManager.entity.Product;
 import org.academiadecodigo.floppybirds.InventoryManager.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
+@RequestMapping("/inv")
 public class ProductController {
 
     @Autowired
--- src/main/java/org/academiadecodigo/floppybirds/InventoryManager/repository/ProductRepository.java
+++ src/main/java/org/academiadecodigo/floppybirds/InventoryManager/repository/ProductRepository.java
@@ -2,10 +2,11 @@ package org.academiadecodigo.floppybirds.InventoryManager.repository;
 
 import org.academiadecodigo.floppybirds.InventoryManager.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends CrudRepository<Product, Integer> {
 
     Product findByName(String name);
 
